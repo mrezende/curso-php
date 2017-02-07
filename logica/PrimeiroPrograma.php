88f561b1ac33a4d2154c5e0af9d4b47f945568c8
Marcelo Martins <marcelo.martins@k19.com.br>
2017-02-06 22:30:18 -0200

    alterando o primeiro programa, removendo o while

--- logica/PrimeiroPrograma.php
+++ logica/PrimeiroPrograma.php
@@ -2,11 +2,6 @@
 
 $contador = 0; // Declaração
 
-while($contador < 100) { // Condição de parada
-  echo "Deu onda" . PHP_EOL;
-  $contador++; // Incremento
-}
-
 for ($contador = 0; $contador < 100; $contador++) {
   echo "Deu onda" . PHP_EOL;
 }
