9a03149c719b5d4b8690b3a1c573364694488691
Marcelo Martins <marcelo.martins@k19.com.br>
2017-02-06 22:17:55 -0200

    primeiro commit

---
+++ logica/PrimeiroPrograma.php
@@ -0,0 +1,12 @@
+<?php
+
+$contador = 0; // Declaração
+
+while($contador < 100) { // Condição de parada
+  echo "Deu onda" . PHP_EOL;
+  $contador++; // Incremento
+}
+
+for ($contador = 0; $contador < 100; $contador++) {
+  echo "Deu onda" . PHP_EOL;
+}
