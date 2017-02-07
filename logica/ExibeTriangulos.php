fa9066f722dc085886dac6c7ec0ffd6a9f4d0c71
Marcelo Martins <marcelo.martins@k19.com.br>
2017-02-07 19:48:46 -0200

    Soluçao do exercício 5

--- logica/ExibeTriangulos.php
+++ logica/ExibeTriangulos.php
@@ -1,22 +1,7 @@
 <?php
 
-for ($i=1; $i < 6; $i++) {
-
-  for ($j=0; $j < $i; $j++) {
-    $linha .= "*"; // $linha = $linha . "*"
-    
-  }
-
-  echo "$linha\n";
-  $linha = "";
+$linha = "*";
+for ($i=0; $i < 5; $i++) {
+  echo "$linha" . PHP_EOL;
+  $linha .= "*";
 }
-
-/*
-
-*
-**
-***
-****
-*****
-
-*/
