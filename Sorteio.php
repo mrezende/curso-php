8c53241dc1f6857e08647f1c679bbefee0f14980
Marcelo Martins <marcelo.martins@k19.com.br>
2017-02-17 21:53:41 -0200

    adicionando sorteio

---
+++ Sorteio.php
@@ -0,0 +1,37 @@
+<?php
+$alunos = [
+  'bkamiya',
+  'caionog1',
+  'eduardoalencarlino',
+  'ezequias-silva',
+  'fabiobellegard',
+  'felipeftrindade',
+  'ffrascino',
+  'gabrielns',
+  'hrvfreitas',
+  'luiscarlosdesouza',
+  'luiz7686',
+  'luizdestefano',
+  'luizhata',
+  'marcelomodesto',
+  'maza-php16',
+  'mirstation',
+  'raphaelettel',
+  'renan03barbosa',
+  'Renat89',
+  'ruansilveira',
+  'tonycasper',
+  'vilela1966',
+  'viniciussanfilippo',
+  'wagnerdeoliveira'
+];
+
+print_r($alunos);
+
+shuffle($alunos);
+
+echo "$alunos[0]" . PHP_EOL;
+
+// Here is a t-shirt coupon code for the GitHub Shop to reward your lucky student:
+
+print_r($alunos);
